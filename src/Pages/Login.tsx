0e3d2438ad2ac7c966bff14ed2e50992a95a9f45
01fe22bca119 <dileepupare8@gmail.com>
2024-05-02 19:10:49 +0530

    Updated few bugs

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,4 @@
-import loginvideo from '../Assets/videos/loginvideo.mp4';
+import loginvideo from '../Assets/videos/loginvideo.webm';
 import { Button } from "@/components/ui/button"
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
